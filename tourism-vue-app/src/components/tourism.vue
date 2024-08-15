ec92741f5a14c9b50286c0e2b063c1d4269b9157
carlospuco <carlos.puco.mb@gmail.com>
2024-08-15 16:11:13 -0500

    Subiendo cambios de front

--- tourism-vue-app/src/components/tourism.vue
+++ tourism-vue-app/src/components/tourism.vue
@@ -15,35 +15,56 @@
 
         <h3></h3>
 
+        <div class="flex space-x-4 mt-4">
+            <DynamicText :text="message" class="flex-1" />
+            <DynamicText :text="message2" class="flex-1" />
+            <DynamicText :text="message" class="flex-1" />
+            <DynamicText :text="message2" class="flex-1" />
+            <DynamicText :text="message2" class="flex-1" />
+        </div>
+        <Map />
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import { getCompletion } from './../services/ia';
+import DynamicText from './DynamicText.vue';
+import Map from './Map.vue';
+
 
 export default defineComponent({
     setup() {
         const prompt = ref('');
         const response = ref('');
         const isLoading = ref(false);
+        const message = ref('');
+        const message2 = ref('');
+
         let language: 'en' | 'es' | 'fr' = 'en';
 
 
         const send = async () => {
             try {
                 isLoading.value = true;
-                const result = await getCompletion(prompt.value);
-                console.log(result);
-                response.value = result.response;
+                message2.value = 'Cargando... enviado';
+                message.value = 'Cargando... enviado1';
+                // const result = await getCompletion(prompt.value);
+                // console.log(result);
+                // response.value = result.response;
                 isLoading.value = false;
+
             } catch (error) {
                 isLoading.value = false;
                 console.error('Error fetching completion:', error);
             }
         };
 
-        return { prompt, response, send, language };
+        return { prompt, response, send, language, message, message2 };
     },
+    components: {
+        DynamicText,
+        Map
+    }
 });
 </script>
--- tourism-vue-app/src/router/index.ts
+++ tourism-vue-app/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../components/Login.vue";
 import Register from "../components/Register.vue";
 import Map from "../components/Map.vue";
+import Tourism from "@/components/tourism.vue";
 
 const routes = [
   {
@@ -22,6 +23,11 @@ const routes = [
     path: "/map",
     name: "Map",
     component: Map,
+  },
+  {
+    path: "/tourism",
+    name: "Tourism",
+    component: Tourism,
   },
 ];
 
