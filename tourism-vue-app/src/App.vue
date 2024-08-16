d01b8ce10c7d14a2f7dfc29ce01b118042439dfb
Anderson <ajr.parra@yavirac.edu.ec>
2024-08-16 11:13:14 -0500

    add layout and sidebar

--- tourism-vue-app/src/App.vue
+++ tourism-vue-app/src/App.vue
@@ -1,10 +1,27 @@
 <template>
-  <router-view />
+  <div class="flex min-h-screen bg-gray-100">
+    <Sidebar />
+    <div class="flex-1 p-6">
+      <div class="flex-1 p-6 ml-64">
+        <Header />
+        <router-view />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import Sidebar from './components/layout/Sidebar.vue';
+import Header from './components/layout/Header.vue';
 
-@Options({})
+@Options({
+  components: {
+    Sidebar,
+    Header,
+  },
+})
 export default class App extends Vue { }
 </script>
+
+<style scoped></style>
---
+++ tourism-vue-app/src/components/layout/Sidebar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="fixed top-0 left-0 h-full w-64 bg-gray-900 text-white flex flex-col overflow-y-auto">
+        <div class="text-xl font-bold p-4">Historial de Busquedas</div>
+        <nav class="flex-1">
+            <ul>
+                <li class="p-4 hover:bg-gray-800 cursor-pointer flex items-center">
+                    <i class="icon-class mr-2"></i>Tables
+                </li>
+                <li class="p-4 hover:bg-gray-800 cursor-pointer flex items-center">
+                    <i class="icon-class mr-2"></i>Forms
+                </li>
+                <li class="p-4 hover:bg-gray-800 cursor-pointer flex items-center">
+                    <i class="icon-class mr-2"></i>UI
+                </li>
+                <li class="p-4 hover:bg-gray-800 cursor-pointer flex items-center">
+                    <i class="icon-class mr-2"></i>Responsive
+                </li>
+                <!-- Agrega más elementos según sea necesario -->
+            </ul>
+        </nav>
+        <div class="p-4 bg-blue-600 hover:bg-blue-500 cursor-pointer">
+            Logout
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Sidebar',
+}
+</script>
